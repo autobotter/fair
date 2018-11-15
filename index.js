406ac944d131afac9d5c78ba805399265423042a
autobotter <autobotter@protonmail.com>
2018-11-14 18:44:16 -0600

    verification steps

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ Steps
 2. Server: Generate random seed (SS) and provide hash (SSH) to client
 3. Client: Create random seed (CS) and send it to server
 4. Server: Combine client seed with server seed and hash result (CSH)
-5. Server: Determine game outcome from CSH and adjust internal values accordingly
-6. Server: Send CSH to client, client will display same game outcome
-7. Client: Verify game outcome
+5. Server: Determine game outcome O from CSH and adjust internal values accordingly
+6. Server: Send O and SS to client
+7. Client: Verify SS hashes to SSH
+8. Client: Calculate CSH from SS and CS
+9. Client: Verify CSH yields same outcome as O
 Subsequent rounds
 Go back to step 3
 */
